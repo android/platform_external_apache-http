e5edcd23f48f7f98af5aa418209c5fe3d9ac7660
Jiyong Park <jiyong@google.com>
2018-04-25 20:05:19 +0900

    org.apache.http.legacy is built with java_sdk_library
    The runtime library, stubs library and droiddoc are all abstracted into
    the java_sdk_library module definition.
    
    note: as a small-side effect, X509TrustManagerExtensions from
    frameworks/base is included as the API signature of the legacy library.
    This is because java_sdk_library includes all java classes from the
    frameworks/base that are in the packages listed in the property
    api_packages.
    
    However, this isn't a breaking change because the added class signature
    is already part of Android API (and we already have duplicated API
    signatures across org.apache.http.legacy and Android API: SslCertficate,
    SslError, etc)
    
    Bug: 77577799
    
    Test: m -j
    
    Change-Id: I30ce741d7dfed1f93d3b19c750dcfbe073cceb69

--- Android.mk
+++ Android.mk
@@ -14,27 +14,13 @@
 #
 LOCAL_PATH:= $(call my-dir)
 
-apache_http_src_files := \
-    $(call all-java-files-under,src) \
-    $(call all-java-files-under,android)
-
-apache_http_java_libs := conscrypt
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := org.apache.http.legacy.boot
-LOCAL_MODULE_TAGS := optional
-LOCAL_JAVA_LIBRARIES := $(apache_http_java_libs)
-LOCAL_SRC_FILES := $(apache_http_src_files)
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_JAVA_LIBRARY)
-
 APACHE_HTTP_LEGACY_API_FILE := $(LOCAL_PATH)/api/apache-http-legacy-current.txt
 APACHE_HTTP_LEGACY_REMOVED_API_FILE := $(LOCAL_PATH)/api/apache-http-legacy-removed.txt
 
 # For unbundled build we'll use the prebuilt jar from prebuilts/sdk.
 ifeq (,$(TARGET_BUILD_APPS)$(filter true,$(TARGET_BUILD_PDK)))
 
-full_classes_jar := $(call intermediates-dir-for,JAVA_LIBRARIES,org.apache.http.legacy,,COMMON)/classes.jar
+full_classes_jar := $(call intermediates-dir-for,JAVA_LIBRARIES,org.apache.http.legacy.stubs,,COMMON)/classes.jar
 # Archive a copy of the classes.jar in SDK build.
 $(call dist-for-goals,sdk win_sdk,$(full_classes_jar):org.apache.http.legacy.jar)
 
@@ -46,9 +32,9 @@ $(call dist-for-goals,sdk win_sdk,$(full_classes_jar):org.apache.http.legacy.jar
 $(eval $(call check-api, \
     check-apache-http-legacy-api-current, \
     $(APACHE_HTTP_LEGACY_API_FILE), \
-    $(INTERNAL_PLATFORM_APACHE_HTTP_API_FILE), \
+    $(INTERNAL_PLATFORM_ORG_APACHE_HTTP_LEGACY_API_FILE), \
     $(APACHE_HTTP_LEGACY_REMOVED_API_FILE), \
-    $(INTERNAL_PLATFORM_APACHE_HTTP_REMOVED_API_FILE), \
+    $(INTERNAL_PLATFORM_ORG_APACHE_HTTP_LEGACY_REMOVED_API_FILE), \
     -error 2 -error 3 -error 4 -error 5 -error 6 \
     -error 7 -error 8 -error 9 -error 10 -error 11 -error 12 -error 13 -error 14 -error 15 \
     -error 16 -error 17 -error 18 -error 19 -error 20 -error 21 -error 23 -error 24 \
@@ -64,11 +50,11 @@ checkapi: check-apache-http-legacy-api
 .PHONY: update-apache-http-legacy-api
 update-api: update-apache-http-legacy-api
 
-update-apache-http-legacy-api: $(INTERNAL_PLATFORM_APACHE_HTTP_API_FILE) | $(ACP)
+update-apache-http-legacy-api: $(INTERNAL_PLATFORM_ORG_APACHE_HTTP_LEGACY_API_FILE) | $(ACP)
 	@echo Copying apache-http-legacy-current.txt
-	$(hide) $(ACP) $(INTERNAL_PLATFORM_APACHE_HTTP_API_FILE) $(APACHE_HTTP_LEGACY_API_FILE)
+	$(hide) $(ACP) $(INTERNAL_PLATFORM_ORG_APACHE_HTTP_LEGACY_API_FILE) $(APACHE_HTTP_LEGACY_API_FILE)
 	@echo Copying apache-http-legacy-removed.txt
-	$(hide) $(ACP) $(INTERNAL_PLATFORM_APACHE_HTTP_REMOVED_API_FILE) $(APACHE_HTTP_LEGACY_REMOVED_API_FILE)
+	$(hide) $(ACP) $(INTERNAL_PLATFORM_ORG_APACHE_HTTP_LEGACY_REMOVED_API_FILE) $(APACHE_HTTP_LEGACY_REMOVED_API_FILE)
 
 endif  # not TARGET_BUILD_APPS
 
